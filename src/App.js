842fd0b89b1188efd68eca8e38750769f0d777fb
Kareine Elecanal <kaelecanal@gmail.com>
2021-06-27 21:24:20 -0700

    Added fonts, additional assets

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import Hours from './components/Hours';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Hours />
+    </div>
+  );
 }
 
 export default App;
