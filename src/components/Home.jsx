5beff94b3d76d63f6af2b2e6427b60ce5a3744d8
jrardd <jrardd@gmail.com>
2021-06-29 12:38:34 -0700

    imported home banner img. transparent navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { Jumbotron } from "reactstrap";
+import "./styles/Home.css";
 
 function Home() {
   return (
-    <div>
-      <h1>This is the Home Page</h1>
-    </div>
+    <>
+      <Jumbotron className="home-header">
+        <h1 className="display-3">This is the Home Page</h1>
+      </Jumbotron>
+    </>
   );
 }
 
