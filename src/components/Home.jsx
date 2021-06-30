3a13d5803f1148645e6f0352550303885173b775
jrardd <jrardd@gmail.com>
2021-06-29 22:12:50 -0700

    Made banner on home page responsive. Started section two of home page with reactstrap grid

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,46 @@
 import React from "react";
-import { Jumbotron } from "reactstrap";
+import { Jumbotron, Container, Row, Col, Button } from "reactstrap";
 import "./styles/Home.css";
 
-function Home() {
+const Section1 = () => {
+  return (
+    <Container className="section1">
+      <Row>
+        <Col>
+          <h1>w e l c o m e</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col></Col>
+        <Col>
+          <Button className="menu-btn">m e n u</Button>
+        </Col>
+        <Col></Col>
+      </Row>
+      <Row>
+        <Col>
+          <h1>
+            t o{"   "}o u r{"   "}s p a c e
+          </h1>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+//const Section2 = () => {};
+
+//const Section3 = () => {};
+
+const Home = () => {
   return (
     <>
       <Jumbotron className="home-header">
-        <h1 className="display-3">This is the Home Page</h1>
+        <h1 className="display-3 welcome-text">w e l c o m e</h1>
       </Jumbotron>
+      <Section1 />
     </>
   );
-}
+};
 
 export default Home;
