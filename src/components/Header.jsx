2d6f177901bbb406a3fd31e0e8b3e9d5febf9002
jrardd <jrardd@gmail.com>
2021-07-05 19:47:10 -0700

    Centered description text in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ function Header({ title, description }) {
   return (
     <Jumbotron className="header-component">
       <h1>{title}</h1>
-      <h3>{description}</h3>
+      <h3 className="header-description">{description}</h3>
     </Jumbotron>
   );
 }
