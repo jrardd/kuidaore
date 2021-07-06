26342ec0e01bf0797c0fe0c582fc0e74f7c6c404
jrardd <jrardd@gmail.com>
2021-07-05 19:38:07 -0700

    header component now uses text from state rather than text saved into image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,12 @@ import floral from "../assets/icons/header-golden-floral.png";
 import "./styles/Header.css";
 
 function Header({ title, description }) {
-  return <Jumbotron className="header-component" />;
+  return (
+    <Jumbotron className="header-component">
+      <h1>{title}</h1>
+      <h3>{description}</h3>
+    </Jumbotron>
+  );
 }
 
 export default Header;
